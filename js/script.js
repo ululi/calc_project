dca697e1a8212dabfd17896e2d744afe43edc697
abc <abc@example.com>
2020-11-04 16:14:36 +0000

    update js file

--- js/script.js
+++ js/script.js
@@ -1,48 +1,27 @@
-// global variables declared
-function myCalc(){
-    let btn;
-    let answer;
-    let body = document.querySelector('body');
-    body.addEventListener('click', function (event) {
-        if (event.target.nodeName == 'BUTTON') {
-            btn = event.target.textContent
-            let displayResult = document.getElementById('display-result');
-            // output.innerHTML = answer
-            if (btn == "+") {
-                displayResult.innerHTML = btn
-            } else if (btn == "/") {
-                displayResult.innerHTML = btn
-            } else if (btn == "x") {
-                displayResult.innerHTML = btn
-            } else if (btn == "-") {
-                displayResult.innerHTML = btn
-            } else if (btn == "1") {
-                displayResult.innerHTML = btn
-            } else if (btn == "2") {
-                displayResult.innerHTML = btn
-            } else if (btn == "3") {
-                displayResult.innerHTML = btn
-            } else if (btn == "4") {
-                displayResult.innerHTML = btn
-            } else if (btn == "5") {
-                displayResult.innerHTML = btn
-            } else if (btn == "6") {
-                displayResult.innerHTML = btn
-            } else if (btn == "7") {
-                displayResult.innerHTML = btn
-            } else if (btn == "8") {
-                displayResult.innerHTML = btn
-            } else if (btn == "9") {
-                displayResult.innerHTML = btn
-            } else if (btn == "0") {
-                displayResult.innerHTML = btn
-            } else if (btn == ".") {
-                displayResult.innerHTML = btn
-            } else if (btn == "=") {
-                displayResult.innerHTML = btn
-            }
-            
-        }
-    });
-}
-myCalc();
+// querySelect all the buttons from the Dom
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const deleteButton = document.getElementById('delete');
+const clearButton = document.getElementById('clear');
+const equalButton = document.querySelector('#equals');
+
+numberButtons.forEach(button=>{
+   button.addEventListener('click',e=>{
+       console.log(e.target.textContent)
+   })
+});
+operatorButtons.forEach((operator)=>{
+    operator.addEventListener('click',e=>{
+        console.log(e.target.textContent)
+    })
+});
+deleteButton.addEventListener('click',e=>{
+    console.log(e.target.textContent)
+});
+clearButton.addEventListener('click',e=>{
+    console.log(e.target.textContent)
+});
+equalButton.addEventListener('click',e=>{
+    console.log(e.target.textContent)
+});
