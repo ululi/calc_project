1a011eaf98bb1ca7746d91a42721d128d0ebb747
abc <abc@example.com>
2020-11-04 18:07:40 +0000

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
-// querySelect all the buttons from the Dom
+class Calculator{
+    constructor(){
+
+    }
+    clear(){
+
+    }
+    delete(){
+
+    }
+    chooseOperator(){
+
+    }
+    compute(){
+
+    }
+}
+
+// Select all the buttons from the Dom
 
 const numberButtons = document.querySelectorAll('[data-number]');
 const operatorButtons = document.querySelectorAll('[data-operator]');
 const deleteButton = document.getElementById('delete');
 const clearButton = document.getElementById('clear');
 const equalButton = document.querySelector('#equals');
+
+// for each button add an event listener
 
 numberButtons.forEach(button=>{
    button.addEventListener('click',e=>{
@@ -25,3 +45,6 @@ clearButton.addEventListener('click',e=>{
 equalButton.addEventListener('click',e=>{
     console.log(e.target.textContent)
 });
+
+// create a new class using the constructor function from up above
+const calculator = new Calculator()
